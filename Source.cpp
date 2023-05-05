e168b879a2436a8baf6f65ff6420ca835382e3db
XyToRr <132078529+XyToRr@users.noreply.github.com>
2023-05-05 17:36:44 +0300

    Add files via upload

--- Source.cpp
+++ Source.cpp
@@ -1,89 +1,409 @@
 #include <iostream>
-#include <Windows.h>
+#include <math.h>
 #include <time.h>
+#include <Windows.h>
 
 using namespace std;
 
 #define S 5
 
-void PrintArray(int* p, int size);
+void PrintArray(int** arr, int size1, int size2);
 
-int* Delete(int* arr, int* size, int x)
+
+int** GetMemory(int** arr, int rows, int cols)
 {
-	int* temp = new int[*size];
-	for (int i = 0; i < *size; i++)
+	if (rows == 0 || cols == 0)
 	{
-		temp[i] = arr[i];
+		return nullptr;
 	}
-	delete[] arr;
-	int oldSize = *size;
-	(*size)--;
-
-	arr = new int[*size];
-	
-	int nextI = 0;
-	for (int i = 0; i < oldSize; i++)
+	if (arr == nullptr)
 	{
-		if (i != x)
+		arr = new int* [rows];
+		for (int i = 0; i < rows; i++)
 		{
-			arr[nextI] = temp[i];
-			nextI++;
+			arr[i] = new int[cols];
 		}
 	}
-	delete[] temp;
-
-	cout << "Del - ";
-	//PrintArray(arr, *size);
 	return arr;
 }
 
-int* DeleteArrPovtor(int* arr, int* size) //видаляти по індексу
+int** RemoveRow(int** arr, int* row, int* col, int index)
 {
-	for (int i = 0; i < *size - 1; i++)
+	if (*row != 0 || *col != 0)
 	{
-		for (int j = i + 1; j < *size; j++)
+		int** temp = nullptr;
+		temp = GetMemory(temp, *row, *col);
+		for (int i = 0; i < *row; i++)
 		{
-			if (arr[i] == arr[j])
+			for (int j = 0; j < *col; j++)
 			{
-				while (arr[i] == arr[j])
-				{
-					arr = Delete(arr, size,j);
-					PrintArray(arr, *size);
-				}
+				temp[i][j] = arr[i][j];
 			}
 		}
+		for (int i = 0; i < *row; i++)
+		{
+			delete[] arr[i];
+		}
+		delete[] arr;
+		arr = nullptr;
+
+		int oldRow = *row;
+		(*row)--;
+		arr = GetMemory(arr, *row, *col);
+
+		int nextI = 0;
+		for (int i = 0; i < oldRow; i++)
+		{
+			//nextI = 0;
+			for (int j = 0; j < *col; j++)
+			{		
+				if (i != index)
+				{
+					arr[nextI][j] = temp[i][j];
+				}
+			}
+			if (i != index)
+			{
+				nextI++;
+			}
+			
+		}
 	}
+	//PrintArray(arr, *row, *col);
 	return arr;
 }
 
-void InitArray(int* p, int size)
+int** RemoveColumn(int** arr, int* row, int* col, int index)
 {
-	for (int i = 0; i < size; i++)
+	if (*row != 0 || *col != 0)
 	{
-		p[i] = rand() % 15;
+		int** temp = nullptr;
+		temp = GetMemory(temp, *row, *col);
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = 0; j < *col; j++)
+			{
+				temp[i][j] = arr[i][j];
+			}
+		}
+		for (int i = 0; i < *row; i++)
+		{
+			delete[] arr[i];
+		}
+		delete[] arr;
+		arr = nullptr;
+
+		int oldCol = *col;
+		(*col)--;
+		arr = GetMemory(arr, *row, *col);
+
+		int nextJ = 0;
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = 0; j < oldCol; j++)
+			{
+				if (j != index)
+				{
+					arr[i][nextJ] = temp[i][j];
+					nextJ++;
+					//PrintArray(arr, *row, *col);
+				}
+			}
+			nextJ = 0;
+		}
+	}
+	//PrintArray(arr, *row, *col);
+	return arr;
+}
+
+int** AddColumn(int** arr, int* row, int* col, int n, int index)
+{
+	int** temp = nullptr;
+	if (*col == 0)
+	{
+		*row = 1;
+		*col = 1;
+		arr = new int*[1];
+		arr[0] = new int[1];
+		arr[0][0] = n;
+	}
+	else
+	{
+		//memory
+		temp = GetMemory(temp, *row, *col);
+		
+
+		//copy
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = 0; j < *col; j++)
+			{
+				temp[i][j] = arr[i][j];
+			}
+			
+		}
+
+		//delete original
+		for (int i = 0; i < *row; i++)
+		{
+			delete[] arr[i];
+		}
+		delete[] arr;
+		arr = nullptr;
+		(*col)++;
+		
+		arr = GetMemory(arr, *row, *col);
+
+
+		//copy back to original
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = 0; j < *col - 1; j++)
+			{		
+					arr[i][j] = temp[i][j];			
+			}
+		}
+
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = *col - 1; j > index; j--)
+			{
+				arr[i][j] = arr[i][j - 1];
+			}
+		}
+
+		for (int i = 0; i < *row; i++)
+		{
+			arr[i][index] = n;
+		}
+		
+		return arr;
 	}
 }
 
-void PrintArray(int* p, int size)
+int** AddRow(int** arr, int* row, int* col, int n, int index)
 {
-	for (int i = 0; i < size; i++)
+	int** temp = nullptr;
+	if (*col == 0)
 	{
-		cout << p[i] << " ";
+		*row = 1;
+		*col = 1;
+		arr = new int* [1];
+		arr[0] = new int[1];
+		arr[0][0] = n;
+	}
+	else
+	{
+		//memory
+		temp = GetMemory(temp, *row, *col);
+
+
+		//copy
+		for (int i = 0; i < *row; i++)
+		{
+			for (int j = 0; j < *col; j++)
+			{
+				temp[i][j] = arr[i][j];
+			}
+
+		}
+
+		//delete original
+		for (int i = 0; i < *row; i++)
+		{
+			delete[] arr[i];
+		}
+		delete[] arr;
+		arr = nullptr;
+		(*row)++;
+
+		arr = GetMemory(arr, *row, *col);
+
+
+		//copy back to original
+		for (int i = 0; i < *row - 1; i++)
+		{
+			for (int j = 0; j < *col; j++)
+			{
+				arr[i][j] = temp[i][j];
+			}
+		}
+
+		for (int i = *row - 1; i > index; i--)
+		{
+			for (int j = 0; j<*col; j++)
+			{
+				arr[i][j] = arr[i-1][j];
+			}
+		}
+
+		for (int j = 0; j < *col; j++)
+		{
+			arr[index][j] = n;
+		}
+
+		return arr;
+	}
+}
+
+void InitArray(int** arr, int size1, int size2)
+{
+	for (int i = 0; i < size1; i++)
+	{	
+		for (int j = 0; j < size2; j++)
+		{
+			arr[i][j] = rand() % 10;
+		}
+		
+	}
+}
+
+void PrintArray(int** arr, int size1,int size2)
+{
+	for (int i = 0; i < size1; i++)
+	{
+		for (int j = 0; j < size2; j++)
+		{
+			cout << arr[i][j] <<" ";
+		}
+		cout << endl;
 	}
 	cout << endl;
 }
 
-
 int main()
 {
 	srand(time(0));
-	
-	int size = 15;
-	int* arr = new int[size];
-	InitArray(arr, size);
-	PrintArray(arr, size);
 
-	arr = DeleteArrPovtor(arr, &size);
-	PrintArray(arr, size);
-	delete[] arr;
+	int rows = 3, cols = 3, n, index;
+	int** arr = nullptr;
+	bool exit = false;
+	char choose;
+	
+	arr = GetMemory(arr, rows, cols);
+
+	InitArray(arr, rows, cols);
+	PrintArray(arr, rows, cols);
+
+	
+
+	while (!exit)
+	{
+		cout << "What to do?" << endl;
+		cout << "a. Add column\nb. Remove column\nc. Add row\nd. Remove row\ne. Exit" << endl;
+		cout << "-";
+		while (true)
+		{
+			cin >> choose;
+			if ((int)choose >= 97 && (int)choose <= 101)
+			{
+				break;
+			}
+			else
+			{
+				cout << "Incorrect input" << endl;
+				cout << "What to do?" << endl;
+				cout << "-";
+			}
+		}
+
+
+		switch (choose)
+		{
+			case 'a':
+				cout << "Number to fill - ";
+				cin >> n;
+				cout << "Index to paste - ";
+				while (true)
+				{
+					cin >> index;
+					if (index >= 0 && index <= cols)
+					{
+						break;
+					}
+					else
+					{
+						cout << "Incorrect" << endl;
+					}
+				}
+				arr = AddColumn(arr, &rows, &cols, n, index);
+				PrintArray(arr, rows, cols);
+				break;
+			case 'b':
+				if (cols != 0)
+				{
+
+
+					cout << "Index to remove - ";
+					while (true)
+					{
+						cin >> index;
+						if (index >= 0 && index < cols)
+						{
+							break;
+						}
+						else
+						{
+							cout << "Incorrect" << endl;
+						}
+					}
+
+					arr = RemoveColumn(arr, &rows, &cols, index);
+					PrintArray(arr, rows, cols);
+					
+				}
+				else
+				{
+					cout << "Massive is already empty" << endl;
+				}
+				break;
+			case 'c':
+				cout << "Number to fill - ";
+				cin >> n;
+				cout << "Index to paste - ";
+				while (true)
+				{
+					cin >> index;
+					if (index >= 0 && index <= rows)
+					{
+						break;
+					}
+					else
+					{
+						cout << "Incorrect" << endl;
+					}
+				}
+				arr = AddRow(arr, &rows, &cols, n, index);
+				PrintArray(arr, rows, cols);
+				break;
+			case 'd':
+				if (rows != 0)
+				{
+					cout << "Index to remove - ";
+					while (true)
+					{
+						cin >> index;
+						if (index >= 0 && index < rows)
+						{
+							break;
+						}
+						else
+						{
+							cout << "Incorrect" << endl;
+						}
+					}
+					arr = RemoveRow(arr, &rows, &cols, index);
+					PrintArray(arr, rows, cols);
+					
+				}
+				else
+				{
+					cout << "Massive is already empty" << endl;
+				}
+				break;
+			case 'e':
+				exit = true;
+				break;
+		}
+	}
 }
